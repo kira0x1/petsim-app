5f8743442b4c48c9bc8157086e6f18ade13da9c9
Kira <kira.eva@protonmail.com>
2023-02-01 18:25:13 +0000

    Create Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,30 @@
 import styles from "@/styles/Home.module.css";
-import { Inter } from "@next/font/google";
 import Head from "next/head";
 import Link from "next/link";
+import { Inter } from "@next/font/google";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <Layout home>
       <Head>
-        <title>PetSim</title>
+        <title>{siteTitle}</title>
         <meta name="description" content="A Pet Simulator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <section className={utilStyles.headingMid}>
+        <p>meow</p>
+        <p>nya</p>
+      </section>
       <div className={styles.main}>
         <h1>
           read <Link href="/posts/first_post">meow</Link>
         </h1>
       </div>
-    </>
+    </Layout>
   );
 }
--- src/pages/posts/first_post.tsx
+++ src/pages/posts/first_post.tsx
@@ -1,13 +1,17 @@
-import styles from "@/styles/Home.module.css";
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <div className={styles.main}>
-      <h1 className="title">meow this is my post</h1>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>meow this is my post</h1>
       <h2>
         <Link href="/">Home</Link>
       </h2>
-    </div>
+    </Layout>
   );
 }
